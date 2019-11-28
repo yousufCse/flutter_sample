f55f7b7678b56ab315931c6ffe1b4ef575a932e9
Md. Yousuf Ali <myali@yousuf-MacBook-Pro.local>
2019-11-28 16:03:29 +0600

    Working with animation(21 no video)

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -5,17 +5,68 @@ class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<Home> {
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(parent: catController, curve: Curves.easeIn),
+    );
+  }
+
+  onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      catController.reverse();
+    } else if (catController.status == AnimationStatus.dismissed) {
+      catController.forward();
+    }
+  }
+
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Animation'),
       ),
-      body: buildAnimation(),
+      body: GestureDetector(
+        child: Center(
+          child: Stack( 
+            children: <Widget>[
+              buildBox(),
+              buildCatAnimation(),
+            ],
+          ),
+        ),
+        onTap: onTap,
+      ),
     );
   }
 
-  Widget buildAnimation() {
-    return Cat();    
+  Widget buildCatAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Positioned(
+          child: child,
+          bottom: catAnimation.value,
+        );
+      },
+      child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.brown,
+    );
   }
 }
