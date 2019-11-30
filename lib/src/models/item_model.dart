94b223aa89697493fff7c888979826f8ce6ced3f
Md. Yousuf Ali <yousuf@Yousufs-MacBook-Pro.local>
2019-11-30 21:59:56 +0600

    Created some classes.

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
@@ -29,4 +31,39 @@ class ItemModel {
         score = parsedJson['score'],
         title = parsedJson['title'],
         descendants = parsedJson['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> parsedJson)
+      : id = parsedJson['id'],
+        deleted = parsedJson['deleted'] == 1,
+        type = parsedJson['type'],
+        by = parsedJson['by'],
+        time = parsedJson['time'],
+        text = parsedJson['text'],
+        dead = parsedJson['dead'] == 1,
+        parent = parsedJson['parent'],
+        poll = parsedJson['pool'],
+        kids = jsonDecode(parsedJson['kids']),
+        url = parsedJson['url'],
+        score = parsedJson['score'],
+        title = parsedJson['title'],
+        descendants = parsedJson['descendants'];
+
+  Map<String, dynamic> toMapForDb() {
+    return <String, dynamic>{
+      'id': id,
+      'type': type,
+      'by': by,
+      'time': time,
+      'parent': parent,
+      'text': text,
+      'poll': poll,
+      'url': url,
+      'score': score,
+      'title': title,
+      'descendants': descendants,
+      'dead': dead ? 1 : 0,
+      'deleted': deleted ? 1 : 0,
+      'kids': jsonEncode(kids),
+    };
+  }
 }
---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import '../models/item_model.dart';
+import 'package:sqflite/sqflite.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  void init() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentDirectory.path, 'items.db');
+
+    db = await openDatabase(path, version: 1,
+        onCreate: (Database newDb, int version) {
+      newDb.execute("""
+         CREATE TABLE Items
+          (
+            id INTEGER PRIMARY KEY,
+            type TEXT,
+            by TEXT,
+            time INTEGER,
+            text TEXT,
+            parent INTEGER,
+            kids BLOB,
+            dead INTEGER,
+            deleted INTEGER,
+            url TEXT,
+            score INTEGER,
+            title TEXT,
+            descendants INTEGER
+          )
+        """);
+    });
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
+
+  Future<int> addItem(ItemModel item) {
+   return db.insert("Items", item.toMapForDb());
+  } 
+}
