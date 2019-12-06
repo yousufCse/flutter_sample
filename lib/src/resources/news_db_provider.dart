d44e1b388c5cc235a68396f19117a05ec98f366f
Md. Yousuf Ali <yousuf@Yousufs-MacBook-Pro.local>
2019-12-07 00:48:34 +0600

    Added ItemModelt to the database

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -18,9 +18,42 @@ class NewsDbProvider {
       version: 1,
       onCreate: (Database newDb, version) {
         newDb.execute("""
-          CREATE_TABLE
+          CREATE_TABLE Items
+            (
+              id INTEGER PRIMARY KEY,
+              deleted INTEGER,
+              type TEXT,
+              by TEXT,
+              time INTEGER,
+              dead INTEGER,
+              parent INTEGER,
+              kids BLOB,
+              url TEXT,
+              score INTEGER,
+              title TEXT,
+              descendants INTEGER
+            )
           """);
       },
     );
+  }
+
+  fetchItem(int id) async {
+    final maps = await db.query(
+      'Items',
+      columns: null,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
+
+  addItem(ItemModel item) {
+    return db.insert('Items', item.toMapForDb());
   }
 }
