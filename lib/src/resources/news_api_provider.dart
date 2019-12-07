d20b7f159d900c183eac911bac3d4693a079f40b
Md. Yousuf Ali <yousuf@Yousufs-MacBook-Pro.local>
2019-12-07 14:16:12 +0600

    Handled fetchTopIds and fetchItem at respository class

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -7,16 +7,14 @@ final _root = 'https://hacker-news.firebaseio.com/v0';
 class NewsApiProvider {
   final client = Client();
 
-  fetchTopIds() async {
-    final response =
-        await client.get('$_root/topstories');
+  Future<List<int>> fetchTopIds() async {
+    final response = await client.get('$_root/topstories');
     final ids = json.decode(response.body);
-    return ids;
+    return ids.cast<int>();
   }
 
-  fetchItem(int id) async {
-    final response =
-        await client.get('$_root/item/$id');
+  Future<ItemModel> fetchItem(int id) async {
+    final response = await client.get('$_root/item/$id');
     final parsedJson = json.decode(response.body);
     final item = ItemModel.fromJson(parsedJson);
     return item;
--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -8,7 +8,7 @@ import '../models/item_model.dart';
 class NewsDbProvider {
   Database db;
 
-  init() async {
+  void init() async {
     Directory documentDirectory = await getApplicationDocumentsDirectory();
 
     final path = join(documentDirectory.path, 'items.db');
@@ -38,7 +38,7 @@ class NewsDbProvider {
     );
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     final maps = await db.query(
       'Items',
       columns: null,
@@ -53,7 +53,7 @@ class NewsDbProvider {
     return null;
   }
 
-  addItem(ItemModel item) {
+  Future<int> addItem(ItemModel item) {
     return db.insert('Items', item.toMapForDb());
   }
 }
