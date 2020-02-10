1bb3e663d5612900f0dc4f9d3ad270de961915c9
Md. Yousuf Ali <yousuf@Yousufs-MacBook-Pro.local>
2020-02-10 09:34:30 +0600

    Passing data through navigation

--- lib/src/screens/categories_screen.dart
+++ lib/src/screens/categories_screen.dart
@@ -14,6 +14,7 @@ class CategoriesScreen extends StatelessWidget {
         children: DUMMY_CATEGORIES
             .map(
               (item) => CategoryItem(
+                item.id,
                 item.title,
                 item.color,
               ),
