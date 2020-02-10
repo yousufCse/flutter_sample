c8beda7c17a56ca8f89078a9d0cbc11b03fcaafb
Md. Yousuf Ali <yousuf@Yousufs-MacBook-Pro.local>
2020-02-10 09:07:41 +0600

    Added fonts

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,6 +11,25 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Meal App',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 299, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(
+            color: Color.fromRGBO(29, 51, 51, 1)
+          ),
+          body2: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold
+          )
+        )
+      ),
       home: CategoriesScreen(),
     );
   }
