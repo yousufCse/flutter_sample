eb206998ac606896285f025b9f0a0faf6895e220
Md. Yousuf Ali <myali@Yousufs-MacBook-Pro.local>
2023-01-09 17:08:56 +0600

    removes unused import

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,4 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_sample/screens/home_screen.dart';
 
 class App extends StatelessWidget {
