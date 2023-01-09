9b0d16ea936e6f5ddb28bc30a733cecaedbd5265
Md. Yousuf Ali <myali@Yousufs-MacBook-Pro.local>
2023-01-09 12:14:19 +0600

    Delete boilerplate code

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_sample/screens/home_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
