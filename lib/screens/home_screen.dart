3c2d99883bd27b021d8788764f9957435a6055a2
yousufCse <yousuf.csebd@gmail.com>
2023-01-09 19:19:02 +0600

    add scaffold to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Scaffold();
   }
 }
