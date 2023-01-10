a938bba2cf0755ad5003abcbce48f44f7d0f2d9e
yousufCse <yousuf.csebd@gmail.com>
2023-01-10 15:52:05 +0600

    example of glass morphism

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_sample/widgets/glass_morphism.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +13,38 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.amber,
+        body: Stack(
+          children: [
+            Image.network(
+              'https://img.theweek.in/content/dam/week/webworld/feature/lifestyle/2017/december/chicken-biriyani.jpg',
+              fit: BoxFit.fitWidth,
+            ),
+            Column(
+              children: [
+                GlassMoorphism(
+                    blur: 5,
+                    radius: 15,
+                    opacity: 0.3,
+                    child: Container(
+                        height: 100,
+                        width: 200,
+                        child: Center(
+                          child: Text(
+                            'Welcome to Food App',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ))),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
