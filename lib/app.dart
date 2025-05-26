1f54b1ecf3061541789b63995d68815eadbec11a
yousufCse <yousuf.csebd@gmail.com>
2025-05-26 14:05:19 +0600

    feat: add home screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sample/screens/home_screen.dart';
+import 'package:flutter_sample/features/home/presentation/screen/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  MaterialApp(
+    return const MaterialApp(
       home: HomeScreen(),
     );
   }
